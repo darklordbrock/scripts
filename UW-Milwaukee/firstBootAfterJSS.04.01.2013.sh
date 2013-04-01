23b97e3ce89721fa2bd65519a23e024091437189
Kyle Brockman <brockman.kyle@gmail.com>
2013-04-01 12:36:31 -0500

    FirstBoot Scripts
    I have added my firstboot scripts. The before sets the groups and some
    basic information. The second script is used to pull the needed
    policies from jamf, and if there was data backed up, it will be
    restored.

---
+++ UW-Milwaukee/firstBootAfterJSS.04.01.2013.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+######
+# First boot script for JSS Production 
+# After JSS QuickAdd is run. 
+# Built on 03.27.2013
+#
+# Kyle Brockman
+# While working at UW-Milwaukee
+######
+
+# Add the jssworker account to local UWM admin group
+dscl . append /Groups/localuwmadmingroup GroupMembership "jss"
+
+#check for JAMF binary 
+if [[  -f /usr/sbin/jamf ]]; then
+	echo "jamf binary is installed"
+else
+	echo "jamf binary is not installed."
+	exit 1	
+fi
+
+#Set the department in JSS based on machine name
+DEPT=`hostname | sed 's/-/ /g' | awk '{print $1}'`
+/usr/sbin/jamf recon -department $DEPT
+
+#check for any jamf policies for the machine. 
+/usr/sbin/jamf policy
+sleep 30
+
+# Trigger manual Printer installs
+/usr/sbin/jamf policy -trigger printdriverBrother
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverCanon
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverEpson
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverFuji
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverGestetner
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverHP
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverInfo
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverInfoTec
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverLanier
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverLexmark
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverNRG
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverRicoh
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverSamsung
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverSavin
+sleep 2
+/usr/sbin/jamf policy -trigger printdriverXerox
+sleep 2
+
+# Restore user data from Shared dir if a backup is there
+BACKUP=`ls /Users/Shared/`
+
+if [[ "$BACKUP" == "" ]]; then
+	echo "No Backup to restore"
+	exit 0
+else
+	
+	#check if connected to AD
+	ADWORK=`id brockma9`
+	if [[ "$ADWORK" == "id: brockma9: no such user" ]]; then
+		echo "This machine is not connected to AD"
+		exit 1
+	else
+		echo "Connected to AD and moving on. Nothing to see here, move along."
+	fi
+	
+	
+	#Find the users that were on the machine
+	USERZ=`ls /Users/Shared/$BACKUP/ | sed 's/.tar//g'`
+		
+	for U in $USERZ; do
+		#Making the new home folder for the AD user account
+		/System/Library/CoreServices/ManagedClient.app/Contents/Resources/createmobileaccount -n $U
+		/usr/sbin/createhomedir -c -u $U
+		sleep 2
+		
+		if [[ ! -d "/Users/$U" ]]; then
+			echo "Home Directory failed to be made."
+			exit 1
+		else
+			#expand tar of the user to tmp
+			mkdir /tmp/$U
+			tar -x /Users/Shared/$BACKUP/$U -C /tmp/$U
+			
+			#copy the files to the new home folder but not the library folder
+			FILEZ=`ls -a /tmp/$U | grep -v "Library"`
+			for F in $FILEZ; do
+				#Copy the files into the home dir
+				cp -Rf /tmp/$U/$F /Users/$U/
+				#Security remove the files from the tmp location
+				srm -fr /tmp/$U
+			done
+			
+			#Set Permissions on the home folders
+			for P in `ls /Users/ | grep -v Shared | grep -v localadmin` ; do
+				#sets owner and group to user and staff
+				chown -R $P:staff /Users/$P
+				#sets the folder to let the staff users in but no access
+				chmod 740 /Users/$P
+				#sets the user to have full access to the home dir and no one else.
+				chmod -R 700 /Users/$P/*
+				#Access to the public folder
+				chmod 777 /Users/$P/Public
+			done 
+ 		fi		
+		
+	done	
+	
+fi
+
+exit 0
